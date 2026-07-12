68cb939b1a0c3338edc11c4fb08f29feab62c566
jammyIT <hsamsed@gmail.com>
2026-07-12 21:52:10 +0530

    Update app.py

--- flask-proxy/app.py
+++ flask-proxy/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, Response
-import requests
+from flask_cors import CORS   
 
 app = Flask(__name__)
-
+CORS(app) 
 # Replace with your Google Apps Script Web App URL
 GAS_URL = "https://script.google.com/macros/s/AKfycbwbJedA3rIluoThDP3r17JbrotsKpEWqBCppgGhikuSQn1PCydVO_rMj3G0tI65I6NJLw/exec"
 

de0808295d11ee396be8830280bdd732a6ecafdd
jammyIT <hsamsed@gmail.com>
2026-07-16 01:32:49 +0530

    Update admin_routes.py

--- routes/admin_routes.py
+++ routes/admin_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect
-from models.exam_question import ExamQuestion
+from models.exam_questions import ExamQuestion
 from models.db_config import db
 
 admin_bp = Blueprint("admin", __name__, url_prefix="/admin")

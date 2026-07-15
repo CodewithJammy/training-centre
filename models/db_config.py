6a3008cc44651be9a9c99285b0b7801ba597c252
jammyIT <hsamsed@gmail.com>
2026-07-16 01:07:37 +0530

    Update db_config.py

--- models/db_config.py
+++ models/db_config.py
@@ -3,10 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def init_db(app):
-    # Azure SQL connection string
+    # Azure SQL connection string with Entra Integrated Authentication
     app.config['SQLALCHEMY_DATABASE_URI'] = (
-        "mssql+pyodbc://examadmin:YourStrongPassword123@examportal-sqlserver.database.windows.net/ExamDB"
-        "?driver=ODBC+Driver+18+for+SQL+Server"
+        "mssql+pyodbc://@questionbank777.database.windows.net/questionBank"
+        "?driver=ODBC+Driver+18+for+SQL+Server&authentication=ActiveDirectoryIntegrated"
     )
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)

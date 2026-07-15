2df63c5a359bb741b5f7d0befab70b103be03a2f
jammyIT <hsamsed@gmail.com>
2026-07-16 00:17:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, render_template, redirect
 import requests
 from flask_cors import CORS
-from flask import Flask
+
 from db_config import init_db, db
 from routes.admin_routes import admin_bp
 
+# Create Flask app once
 app = Flask(__name__)
+CORS(app)  # adds CORS headers automatically
+
+# Initialize DB
 init_db(app)
 
 # Register the admin blueprint
 app.register_blueprint(admin_bp)
-
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()  # Ensures ExamQuestions table exists
-    app.run(debug=True)
-
-app = Flask(__name__)
-CORS(app)  # adds CORS headers automatically
 
 # Google Apps Script Web App URL
 GAS_URL = "https://script.google.com/macros/s/AKfycbwbJedA3rIluoThDP3r17JbrotsKpEWqBCppgGhikuSQn1PCydVO_rMj3G0tI65I6NJLw/exec"
@@ -43,4 +39,6 @@ def proxy():
     return resp
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()  # Ensures ExamQuestions table exists
     app.run(debug=True)

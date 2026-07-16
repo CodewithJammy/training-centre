bd9b31ed1446de03492760702c82d0b8071b3204
jammyIT <hsamsed@gmail.com>
2026-07-16 18:55:47 +0530

    Update admin_routes.py

--- routes/admin_routes.py
+++ routes/admin_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect,jsonify
 from models.exam_questions import ExamQuestion
 from models.db_config import db
 

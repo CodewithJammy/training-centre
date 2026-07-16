0d9476e720872fa7a77c2ab5a6c8be3ef0bb10fe
jammyIT <hsamsed@gmail.com>
2026-07-16 14:09:18 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+set -e
+
+# Update package lists
 apt-get update
+
+# Install unixODBC
 apt-get install -y unixodbc-dev
-apt-get install -y msodbcsql18
+
+# Pre-accept license for msodbcsql18
+ACCEPT_EULA=Y apt-get install -y msodbcsql18
+
+# Install Python dependencies
 pip install -r requirements.txt
+
+# Start gunicorn
 gunicorn --bind=0.0.0.0:8000 app:app
 

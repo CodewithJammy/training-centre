e5a0d77017d01337ea6c4ba20967a39590193af9
jammyIT <hsamsed@gmail.com>
2026-07-17 14:23:00 +0530

    Update user_login.py

--- models/user_login.py
+++ models/user_login.py
@@ -6,3 +6,5 @@ class UserLogin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    mobile = db.Column(db.String(15), unique=True, nullable=False)

e99f5e41d0b07552f021c9f1aea1fc0a6f2af6ce
jammyIT <hsamsed@gmail.com>
2026-07-18 16:56:56 +0530

    Update db_config.py

--- models/db_config.py
+++ models/db_config.py
@@ -3,7 +3,7 @@ import pyodbc
 
 def get_connection():
     # Read connection string from Azure App Service settings
-    database_url = os.getenv("DATABASE_URL")
+    database_url = os.getenv("db_url")
 
     if not database_url:
         raise RuntimeError("DATABASE_URL is not set in App Service configuration")

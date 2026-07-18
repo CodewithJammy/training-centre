3d849ac9b0eab99c4bb0bff4988432f1fa004b05
jammyIT <hsamsed@gmail.com>
2026-07-18 14:00:28 +0530

    Update db_config.py

--- models/db_config.py
+++ models/db_config.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 def init_db(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = (
         "mssql+pyodbc://CloudSA2ff8cbc8%40questionbank777:Cyber%40%2312345@questionbank777.database.windows.net:1433/questionBank"
-        "?driver=ODBC+Driver+18+for+SQL+Server&Encrypt=yes&TrustServerCertificate=no"
+        "?driver=ODBC+Driver+18+for+SQL+Server&Encrypt=yes&TrustServerCertificate=no&Connection+Timeout=30""
     )
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 

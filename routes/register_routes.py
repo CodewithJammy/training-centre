147bf3791129d88b3985c08b96d914addae6cf77
jammyIT <hsamsed@gmail.com>
2026-07-19 14:34:55 +0530

    Update register_routes.py

--- routes/register_routes.py
+++ routes/register_routes.py
@@ -3,9 +3,6 @@ from flask import Flask, request
 from models.db_config import get_connection
 app = Flask(__name__)
 
-conn_str = "Driver={ODBC Driver 17 for SQL Server};Server=YOUR_SERVER;Database=YOUR_DB;UID=USER;PWD=PASS;"
-def get_connection():
-    return pyodbc.connect(conn_str)
 
 @app.route('/register', methods=['POST'])
 def register():

3bdc50fd0228d01b60aaf1c3feb5693a7aa4dba2
CodewithJammy <CodewithJammy@users.noreply.github.com>
2026-07-25 09:37:24 +0000

    deploy: 70b139596428f001fbea077ee021b038f28a12c7

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def index():
     return render_template("index.html")
 @app.route("/courses")
 def courses():
-    return render_template("templates/exam_dashboard.html")
+    return render_template("exam_dashboard.html")
 # Google Apps Script Web App URL
 GAS_URL = "https://script.google.com/macros/s/AKfycbwbJedA3rIluoThDP3r17JbrotsKpEWqBCppgGhikuSQn1PCydVO_rMj3G0tI65I6NJLw/exec"
 

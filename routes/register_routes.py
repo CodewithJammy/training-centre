cbd4103a75e4d019b7ce3f91406dc156a42b46b7
CodewithJammy <CodewithJammy@users.noreply.github.com>
2026-07-28 11:54:19 +0000

    deploy: 75f282b03003ede0b55a7811ab021751ced88817

--- routes/register_routes.py
+++ routes/register_routes.py
@@ -1,46 +1,47 @@
 import pyodbc
 from flask import Flask, Blueprint, render_template, request, session, redirect, url_for,jsonify
 from models.db_config import get_connection
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 
 register_bp = Blueprint("user", __name__, url_prefix="/user")
 
 
+
 @register_bp.route("/signup", methods=["POST"])
 def signup():
-    username = request.form["name"]
-    email = request.form["email"]
-    mobile = request.form["mobile"]
-    password = request.form["password"]
-    payment_option = request.form["payment_option"]
+    try:
+        username = request.form["name"]
+        email = request.form["email"]
+        mobile = request.form["mobile"]
+        password = request.form["password"]
+        payment_option = request.form["payment_option"]
 
-    # Hash password before storing
-    hashed_password = generate_password_hash(password)
+        hashed_password = generate_password_hash(password)
 
-    # Default values
-    amount = 0
-    expiredate = None
+        amount = 0
+        expiredate = None
 
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute(
-        """
-        INSERT INTO Subscriber (username, email , mobile, password, payment_option, payment_status, amount, date, expiredate)
-        VALUES (?, ?, ?, ?, ?, ?, ?, GETDATE(), ?)
-        """,
-        (username, email , mobile, hashed_password, payment_option, "Pending", amount, expiredate)
-    )
-    conn.commit()
-    conn.close()
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute(
+            """
+            INSERT INTO Subscriber (username, email, mobile, password, payment_option, payment_status, amount, date, expiredate)
+            VALUES (?, ?, ?, ?, ?, ?, ?, GETDATE(), ?)
+            """,
+            (username, email, mobile, hashed_password, payment_option, "Pending", amount, expiredate)
+        )
+        conn.commit()
+        conn.close()
 
-    
-    return jsonify({
+        return jsonify({
             "success": True,
             "message": f"Hello {username}, you are registered successfully! Payment pending."
         })
     except Exception as e:
         return jsonify({"success": False, "message": str(e)}), 400
+
 
 @register_bp.route("/register", methods=["POST"])
 def register():

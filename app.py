8cbaeed1bf4b35b8b4ae938e83284abbb1ac7e30
CodewithJammy <CodewithJammy@users.noreply.github.com>
2026-07-28 13:30:20 +0000

    deploy: 91af9a3058685efbf125e8d91d4b7c4202ed2a21

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'hsamsed@gmail.com'
-app.config['MAIL_PASSWORD'] = 'your_app_password'  # use app password, not your real password
+app.config['MAIL_PASSWORD'] = 'ywow wasr eafb ugzr'  # use app password, not your real password
 app.config['MAIL_DEFAULT_SENDER'] = 'hsamsed@gmail.com'
 
 mail = Mail(app)

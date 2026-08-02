6c8a9df0ec88bb4e0a3e362f1e6b12e95699fc56
CodewithJammy <CodewithJammy@users.noreply.github.com>
2026-08-02 07:59:43 +0000

    deploy: 934f827a635dc3f25cc7d7e936f77a4b881599e2

--- routes/user.py
+++ routes/user.py
@@ -1,7 +1,7 @@
 
 from flask import Blueprint, render_template, request, session, redirect, url_for,jsonify,flash
 from models.db_config import get_connection
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash,check_password_hash
 from models.db_helpers import row_to_dict
 
 

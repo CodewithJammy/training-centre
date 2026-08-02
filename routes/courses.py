431d9dc47ee8f3ec0444a9cbed6866e111cc0f43
CodewithJammy <CodewithJammy@users.noreply.github.com>
2026-08-02 04:05:21 +0000

    deploy: 564fc3de3e2a46f7aa351867063ec01c17e6f00e

--- routes/courses.py
+++ routes/courses.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, session, redirect, url_for,jsonify,flash
 from models.db_config import get_connection
 from werkzeug.security import generate_password_hash
+from models.db_helpers import row_to_dict
 
 # Create connection + cursor
 conn = get_connection()

59421f657dedce7d772f85b6fe0e3ab8783d5d45
CodewithJammy <CodewithJammy@users.noreply.github.com>
2026-08-08 11:47:15 +0000

    deploy: 91237637d228baac94a361540e5659e306ea25f4

--- routes/admin.py
+++ routes/admin.py
@@ -11,7 +11,6 @@ cursor = conn.cursor()
 
 # Admin dashboard route
 @admin_bp.route('/dashboard', methods=['GET'])
-
 def dashboard():
     # Fetch courses
     cursor.execute("SELECT * FROM Courses")
@@ -32,38 +31,49 @@ def dashboard():
         topics=topics
     )
 
-
-
 # Add Course
 @admin_bp.route('/add-course', methods=['POST'])
 def add_course():
     name = request.form['name']
-    desc = request.form['description']
-    cursor.execute("INSERT INTO Courses (Name, Description) VALUES (?, ?)", (name, desc))
+    desc = request.form.get('description')
+    image_url = request.form.get('image_url')
+    price = request.form.get('price')
+    category = request.form.get('category')
+    duration = request.form.get('duration')
+    level = request.form.get('level')
+    syllabus_pdf_url = request.form.get('syllabus_pdf_url')
+
+    cursor.execute("""
+        INSERT INTO Courses (Name, Description, ImageUrl, Price, Category, Duration, Level, SyllabusPdfUrl)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    """, (name, desc, image_url, price, category, duration, level, syllabus_pdf_url))
     conn.commit()
     flash("Course added successfully!", "success")
     return redirect(url_for('admin.dashboard'))
-
 
 # Add Test
 @admin_bp.route('/add-test', methods=['POST'])
 def add_test():
     course_id = request.form['course_id']
     name = request.form['name']
+    price = request.form.get('price')
     test_type = request.form['test_type']
     year = request.form.get('year')
-    cursor.execute("INSERT INTO Tests (CourseId, Name, Test_Type, Year) VALUES (?, ?, ?, ?)",
-                   (course_id, name, test_type, year))
+
+    cursor.execute("""
+        INSERT INTO Tests (CourseId, Name, Price, Test_Type, Year)
+        VALUES (?, ?, ?, ?, ?)
+    """, (course_id, name, price, test_type, year))
     conn.commit()
     flash("Test added successfully!", "success")
     return redirect(url_for('admin.dashboard'))
-
 
 # Add Topic
 @admin_bp.route('/add-topic', methods=['POST'])
 def add_topic():
     test_id = request.form['test_id']
     topic_name = request.form['topic_name']
+
     cursor.execute("INSERT INTO Topics (TestId, Title) VALUES (?, ?)", (test_id, topic_name))
     conn.commit()
     flash("Topic added successfully!", "success")
